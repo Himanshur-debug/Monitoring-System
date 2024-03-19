3608dd7a1fc833eef07d64395a248c3e00580678
Hitesh Kumar <mystfear1377@gmail.com>
2024-03-20 01:59:43 +0530

    add configuration files to get server info, database info, authentication key

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream> 
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include <client.h>
+
+using json = nlohmann::json;
+
+struct ServerConfig {
+    std::string serverIP_;
+    std::string serverPort_;
+    std::string authKey_;
+
+    ServerConfig& operator=(const json& j) {
+        serverIP_ = j.at("serverIP").get<std::string>();
+        serverPort_ = j.at("serverPort").get<std::string>();
+        authKey_ = j.at("authKey").get<std::string>();
+        return *this;
+    }
+    
+};
  
 int main() { 
     try {
+        // reading configuration file config.json
+        std::ifstream configFile("config.json");
+        if (!configFile.is_open()) {
+            throw std::runtime_error("Failed to open Configuration file");
+        }
+        json configJson;
+        configFile >> configJson;
+
+        ServerConfig serverConfig;
+        serverConfig = configJson[0];
+
         Client* client = Client::getInstance();
-        client->initialize("127.0.0.1", "8080", "hello");
+        client->initialize(serverConfig.serverIP_, serverConfig.serverPort_, serverConfig.authKey_);
         client->run();
     }
     catch(const std::exception& e) {
--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,17 +1,55 @@
 #include <iostream> 
-#include <server.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include <boost/asio.hpp>
+#include <server.h>
 #include <DatabaseInitializer.h>
 
 using namespace boost::asio;
+using json = nlohmann::json;
+struct DBConfig {
+    std::string dbAddress_;
+    std::string dbUser_;
+    std::string dbPassword_;
+
+    DBConfig& operator=(const json& j) {
+        dbAddress_ = j.at("dbAddress").get<std::string>();
+        dbUser_ = j.at("dbUser").get<std::string>();
+        dbPassword_ = j.at("dbPassword").get<std::string>();
+        return *this;
+    }
+    
+};
+struct AuthConfig {
+    std::string key_;
+
+    AuthConfig& operator=(const json& j) {
+        key_ = j.at("authKey").get<std::string>();
+        return *this;
+    }
+};
  
 int main() { 
     try {
-        boost::asio::io_context ioc_;
-        DatabaseInitializer dbInitializer("localhost", "root", "hello World @123");
-        dbInitializer.initializeDatabase();
-        Server server(ioc_, "hello", dbInitializer);
+        //reading configuration file config.json
+        std::ifstream configFile("config.json");
+        if (!configFile.is_open()) {
+            throw std::runtime_error("Failed to open Configuration file");
+        }
+        json configJson;
+        configFile >> configJson;
 
+        DBConfig dbConfig;
+        dbConfig = configJson[0];
+        AuthConfig authConfig;
+        authConfig = configJson[1];
+
+        boost::asio::io_context ioc_;
+
+        DatabaseInitializer dbInitializer(dbConfig.dbAddress_, dbConfig.dbUser_, dbConfig.dbPassword_);
+        dbInitializer.initializeDatabase();
+
+        Server server(ioc_, authConfig.key_, dbInitializer);
         ioc_.run();
     }
     catch(const std::exception& e) {
